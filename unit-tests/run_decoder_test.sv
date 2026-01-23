ca4810687df564392d0fd8b205dbe2a544c7820c
Luca <luca@teapot.ovh>
2026-01-23 20:20:01 +0100

    modernize run decoder test SV code

--- unit-tests/run_decoder_test.sv
+++ unit-tests/run_decoder_test.sv
@@ -14,26 +14,19 @@ always_comb sq_wr.tie_off_m();
 always_comb cq_rd.tie_off_s();
 always_comb cq_wr.tie_off_s();
 
-always_comb axis_host_recv[1].tie_off_s();
-always_comb axis_host_send[1].tie_off_m();
-
 /* -- USER LOGIC -------------------------------------------------------- */
 
 /* -- INPUT ------------------------------------------------------------- */
 
-AXI4S #(.AXI4S_DATA_BITS(512)) host_in (.aclk(aclk), .aresetn(aresetn));
-assign axis_host_recv[0].tready = host_in.tready;
-assign host_in.tdata = axis_host_recv[0].tdata;
-assign host_in.tkeep = axis_host_recv[0].tkeep;
-assign host_in.tlast = axis_host_recv[0].tlast;
-assign host_in.tvalid = axis_host_recv[0].tvalid;
+AXI4S axi_host_recv_0 (.aclk(aclk), .aresetn(rst_n));
+`AXIS_ASSIGN(axis_host_recv[0], axi_host_recv_0)
 
 ndata_i #(data8_t, 64) in ();
 AXIToNData #(data8_t, 64) inst_axi_to_ndata (
-    .clk(aclk),
-    .rst_n(aresetn),
+    .clk(clk),
+    .rst_n(rst_n),
 
-    .in(host_in),
+    .in(axi_host_recv_0),
     .out(in)
 );
 
@@ -56,45 +49,19 @@ ReadyValidCyclicDriver #(run_decoder_metadata_t, 3) inst_meta_driver (
 
 /* -- OUTPUT ------------------------------------------------------------ */
 
-integer output_databeat;
-AXI4S #(.AXI4S_DATA_BITS(512)) host_out (.aclk(aclk), .aresetn(aresetn));
-assign host_out.tready = axis_host_send[0].tready;
-assign axis_host_send[0].tdata = host_out.tdata;
-assign axis_host_send[0].tkeep = host_out.tkeep;
-assign axis_host_send[0].tlast = host_out.tlast;
-assign axis_host_send[0].tvalid = host_out.tvalid;
-assign axis_host_send[0].tid = output_databeat;
+AXI4S axi_host_send_0 (.aclk(aclk), .aresetn(rst_n));
+`AXIS_ASSIGN(axi_host_send_0, axis_host_send[0])
 
 ndata_i #(data32_t, 16) out ();
 NDataToAXI #(data32_t, 16) inst_ndata_to_axi (
     .clk(aclk),
-    .rst_n(aresetn),
+    .rst_n(rst_n),
 
     .in(out),
-    .out(host_out)
+    .out(axi_host_send_0)
 );
 
 /* -- DESIGN WIRING ----------------------------------------------------- */
-
-always_ff @(posedge aclk) begin
-    if(aresetn == 1'b0) begin 
-        output_databeat  <= 0;
-    end else begin
-        // if (host_in.tvalid && host_in.tready) begin
-        //     $display("< in valid: %x, ready: %x, last: %x", host_in.tvalid, host_in.tready, host_in.tlast);
-        // end
-
-        if (host_out.tvalid && host_out.tready) begin
-            // $display("> out valid: %x, ready: %x, last: %x, keep: %x", host_out.tvalid, host_out.tready, host_out.tlast, host_out.tkeep);
-            output_databeat <= output_databeat + 1;
-
-            if (host_out.tlast) begin
-              // $display(">>! got tlast after %d databeats", output_databeat+1);
-              output_databeat <= 0;
-            end
-        end
-    end
-end
 
 RunDecoder #(data32_t, 16) inst_run_decoder (
     .clk(aclk),
