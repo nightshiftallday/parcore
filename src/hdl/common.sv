dec7c70740a75105717cf8e95810995714343806
Luca <luca@teapot.ovh>
2026-02-15 16:08:26 +0100

    increase dictionary size to handle pages with dictioanries slightly bigger than 1MiB

--- src/hdl/common.sv
+++ src/hdl/common.sv
@@ -8,11 +8,13 @@ import lynxTypes::*;
 parameter int VARINT_NUM_BYTES = 4;
 parameter int VARINT_NUM_BITS = VARINT_NUM_BYTES * 7;
 parameter int VARINT_LENGTH_BITS = $clog2(VARINT_NUM_BYTES);
-parameter int BPE_MASK_SIZE = 18;
+parameter int ID_BITS = 19;
+parameter int BPE_MASK_SIZE = ID_BITS;
+// parameter int BPE_MASK_SIZE = 18;
 
 // We want to have 1MiB dictionaries. That would take 20 bits to index fully.
 // Since the TypedDictionary uses 32bit elements (4 bytes), we take 2 bits of (log2(4)).
-typedef logic [BPE_MASK_SIZE - 1:0] id_t;
+typedef logic [ID_BITS - 1:0] id_t;
 
 typedef enum logic {
     COMPRESSION_RAW = 0,
