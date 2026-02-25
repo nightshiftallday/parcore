067d4cf86abe20dad330a6f57f03ad38dd38c7fa
Luca <luca@teapot.ovh>
2026-02-25 09:37:10 +0100

    add stream profiler to measure module performance

--- src/vfpga_top.svh
+++ src/vfpga_top.svh
@@ -19,12 +19,12 @@ assign clk   = aclk;
 assign rst_n = aresetn;
 
 /* -- CONFIG ------------------------------------------------------------ */
-write_config_i write_configs[3](.*);
-read_config_i  read_configs [3](.*);
+write_config_i write_configs[4](.*);
+read_config_i  read_configs [4](.*);
 GlobalConfig #(
     .SYSTEM_ID(PARCORE_SYSTEM_ID),
-    .NUM_CONFIGS(3),
-    .ADDR_SPACE_SIZES({N_STREAMS, COLUMN_CHUNK_DECODER_CONFIG_REGS*N_STREAMS, PAGE_DECODER_CONFIG_REGS*N_STREAMS})
+    .NUM_CONFIGS(4),
+    .ADDR_SPACE_SIZES({N_STREAMS+1, COLUMN_CHUNK_DECODER_CONFIG_REGS*N_STREAMS, PAGE_DECODER_CONFIG_REGS*N_STREAMS, 8*N_STREAMS+1})
 ) inst_config (
     .clk(clk),
     .rst_n(rst_n),
@@ -75,6 +75,28 @@ PageDecoderConfig #(
     .out(page_conf)
 );
 
+// -- Profiling ------------------------------------------------------------------------------------
+
+// We use this for profiling in the output buffer manager software example
+data64_t perf_counters[8 * N_STREAMS];
+data64_t write_regs[1];
+GenericConfig #(
+    .NUM_READ_REGISTERS(8 * N_STRM_AXI),
+    .NUM_WRITE_REGISTERS(1)
+) inst_generic_config (
+    .clk(clk),
+    .rst_n(rst_n),
+
+    .write_config(write_configs[3]),
+    .read_config(read_configs[3]),
+
+    .in(perf_counters),
+    .out(write_regs)
+);
+
+data64_t num_inputs;
+assign num_inputs = write_regs[0];
+
 AXI4S outputs [N_STREAMS](.aclk(clk), .aresetn(rst_n));
 
 generate
@@ -121,6 +143,64 @@ generate
 
             .in(in),
             .out(out)
+        );
+
+        /* -- PROFILING --------------------------------------------------------- */
+        data64_t inputs_counter, n_inputs_counter;
+        logic    is_stop;
+
+        always_ff @(posedge clk) begin
+            if (!rst_n) begin
+                inputs_counter <= '0;
+            end else begin
+                inputs_counter <= n_inputs_counter;
+            end
+        end
+
+        always_comb begin
+            n_inputs_counter = inputs_counter;
+            is_stop = 1'b0;
+
+            if (in.valid && in.last && in.ready) begin
+                if (inputs_counter == num_inputs - 1) begin
+                    n_inputs_counter = '0;
+                    is_stop = 1'b1;
+                end else begin
+                    n_inputs_counter = inputs_counter + 1;
+                end
+            end
+        end
+
+        StreamProfiler inst_perf_counter_in (
+            .clk(clk),
+            .rst_n(rst_n),
+
+            .last (in.last),
+            .valid(in.valid),
+            .ready(in.ready),
+
+            .stop(is_stop_in),
+
+            .handshakes_cycles(perf_counters[8 * I + 0]),
+            .starved_cycles   (perf_counters[8 * I + 1]),
+            .stalled_cycles   (perf_counters[8 * I + 2]),
+            .idle_cycles      (perf_counters[8 * I + 3])
+        );
+
+        StreamProfiler inst_perf_counter_out (
+            .clk(clk),
+            .rst_n(rst_n),
+
+            .last (out.last),
+            .valid(out.valid),
+            .ready(out.ready),
+
+            .stop(out.valid && out.last && out.ready),
+
+            .handshakes_cycles(perf_counters[8 * I + 4]),
+            .starved_cycles   (perf_counters[8 * I + 5]),
+            .stalled_cycles   (perf_counters[8 * I + 6]),
+            .idle_cycles      (perf_counters[8 * I + 7])
         );
 
         // ila_top inst_ila_top (
