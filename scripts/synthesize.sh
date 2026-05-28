4a0a05183c3418add48b22e24d8d138c954c23d5
Jonas Dann <jonas.dann@inf.ethz.ch>
2026-05-28 13:18:00 +0200

    Removed Coyote as submodule and instead use the version of libSTF

--- scripts/synthesize.sh
+++ scripts/synthesize.sh
@@ -1,38 +1,17 @@
-#/bin/bash 
+#!/bin/bash
 
-#source contents for notification
-if [ ! -x "./webhook.sh" ]; then
-    echo "Webhook file could not be found or was not executable!"
-    exit 1
-fi
+pushd hardware
 
-# Script that will run the synthesis and send a webhook notification when it finished or failed.
-SYNTH_DIR=build_synth
-# Delete old builds
-echo "CLEANING UP OLD BUILD DIRECTORY"
-rm -r -f $SYNTH_DIR
+# Finds the build directory with the highest number and starts the synthesis in a new directory with that number + 1
+n=0
+for d in build-[0-9][0-9]; do
+    [ -d "$d" ] || continue
+    num="${d#build-}"
+    [ "$((10#$num))" -gt "$n" ] && n=$((10#$num))
+done
+build_dir="build-$(printf '%02d' $((n + 1)))"
+echo Building bitstream in hardware/$build_dir...
 
-# Run sim setup
-echo "EXECUTING SETUP"
-mkdir $SYNTH_DIR
-pushd $SYNTH_DIR
-/usr/bin/cmake ..
-make project
-make sim
-popd
-
-# Synthesize
-./webhook.sh "started"
-pushd $SYNTH_DIR
-echo "RUNNING SYNTHESIS"
-make bitgen 2>&1 | tee synth.out
-EXIT_CODE=$?
-popd
-
-# Call web hook
-DIR=$(pwd)
-LAST_LINES=$(tail -n 10 $SYNTH_DIR/synth.out | cat)
-LAST_WNS=$(grep -nE "WNS=-?[0-9]+(\.[0-9]+)?" $SYNTH_DIR/synth.out | tail -1)
-MESSAGE="DIR: $DIR"$'\n'$'\n'"$LAST_LINES"$'\n'$'\n'"LAST WNS"$'\n'$'\n'"$LAST_WNS"
-
-./webhook.sh "finished" "$EXIT_CODE" "$MESSAGE"
+mkdir "$build_dir"
+cmake -S . -B "$build_dir" "${cmake_args[@]}"
+tmux new-session -d -s "bitgen-$build_dir" "cmake --build $build_dir --target project --target bitgen &> $build_dir/bitgen.log"
